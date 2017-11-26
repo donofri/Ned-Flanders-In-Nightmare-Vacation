4bc96b76bcc2d96bf103ec2e0aabf975cd985dcd
Thomas Donofri <34009413+donofri@users.noreply.github.com>
2017-11-26 16:46:19 -0500

    Update CustomFiles.lua

--- CustomFiles.lua
+++ CustomFiles.lua
@@ -2,11 +2,8 @@
 ModPath = GetModPath()
 Resources = ModPath .. "/Resources"
 hudtype = GetSetting("HUDType")
---radio = GetSetting("Radio")
---difficulty = GetSetting("Difficulty") + 1
 
 -- Load MFK Functions
 dofile(Resources .. "/lib/mfk.lua")
---dofile(Resources .. "/hudtype.lua")
 dofile(Resources .. "/scripts/classes/mode.lua")
 dofile(Resources .. "/radio.lua")
